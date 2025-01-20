512adb52377044aec33257f25cd7f2b4f84b1aab
Andrey Glumov <gluman@yandex.ru>
2025-01-21 00:08:30 +0300

    final commit today

--- task.js
+++ task.js
@@ -1,18 +1,26 @@
-const rotator = document.querySelectorAll('class')
+const rotator = document.querySelectorAll('span')
+// const rotator = document.getElementsByClassName('rotator_case')
+console.log(rotator)
+function rotate(r, index) {
+    r.classList.add
 
-setInterval(() => {
-    rotator.forEach(function (r) {
-        if (r.classList.contains('rotator_case')) {
-                r.classList.add('rotator__case_active')
-                console.log(r.textContent)
-                r_speed = r.getAttribute('data-speed')
-                console.log(r_speed)
-                setTimeout(() => {
-                    r.classList.remove('rotator__case_active')
-                }, r_speed)
-        }}
-        )
 }
+setInterval(() => {
 
-
+    random_index = random.nextInt(r.lenght)
+    console.log(random_index)
+    rotator.forEach((r) => {
+        if (r.classList.contains('rotator_case')) {
+            r.classList.add('rotator__case_active')
+            console.log(r.textContent)
+            r_speed = r.getAttribute('data-speed')
+            console.log(r_speed)
+            // setTimeout(() => {
+            //     r.classList.remove('rotator__case_active')
+            // }, r_speed)
+            r.classList.remove('rotator__case_active')
+        }
+    }
+    )
+}
     , 1000)
