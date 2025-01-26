25c446ec7b7fa684a05c8068f044255e5bce9bb8
Andrey Glumov <gluman@yandex.ru>
2025-01-26 18:52:08 +0300

    final commit tiday

--- task.js
+++ task.js
@@ -1,26 +1,20 @@
-const rotator = document.querySelectorAll('span')
-// const rotator = document.getElementsByClassName('rotator_case')
-console.log(rotator)
-function rotate(r, index) {
-    r.classList.add
-
-}
+const rotator = document.getElementsByClassName('rotator__case')
+let r_speed = 1000
 setInterval(() => {
-
-    random_index = random.nextInt(r.lenght)
-    console.log(random_index)
-    rotator.forEach((r) => {
-        if (r.classList.contains('rotator_case')) {
-            r.classList.add('rotator__case_active')
-            console.log(r.textContent)
-            r_speed = r.getAttribute('data-speed')
-            console.log(r_speed)
-            // setTimeout(() => {
-            //     r.classList.remove('rotator__case_active')
-            // }, r_speed)
-            r.classList.remove('rotator__case_active')
+    let random_index = Math.floor(Math.random() * rotator.length)
+    let r_color = 'black'
+    console.log("random index:", random_index)
+    for (i = 0; i < rotator.length; i++) {
+        rspan = rotator[i]
+        rspan.classList.remove('rotator__case_active')
+        if (random_index === i) {
+            r_speed = rspan.getAttribute('data-speed')
+            r_color = rspan.getAttribute('data-color')
         }
     }
-    )
+    r = rotator[random_index]
+    r.classList.add('rotator__case_active')
+    r.setAttribute('style', `color: ${r_color}`);
+    console.log(r)
 }
-    , 1000)
+    , r_speed);
